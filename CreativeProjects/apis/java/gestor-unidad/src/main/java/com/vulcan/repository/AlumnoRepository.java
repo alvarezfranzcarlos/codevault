d2bd3c7082b6402c80d58cd8a8de822c0e1bde90
AlvarezFranz <alvarez.franz.carlos@gmail.com>
2025-01-27 22:38:45 -0400

    Remove gestor-unidad from the main repo

--- CreativeProjects/apis/java/gestor-unidad/src/main/java/com/vulcan/repository/AlumnoRepository.java
+++
@@ -1,9 +0,0 @@
-package com.vulcan.repository;
-
-import com.vulcan.model.Alumno;
-import io.quarkus.hibernate.orm.panache.PanacheRepository;
-import jakarta.enterprise.context.ApplicationScoped;
-
-@ApplicationScoped
-public class AlumnoRepository implements PanacheRepository<Alumno> {
-}
